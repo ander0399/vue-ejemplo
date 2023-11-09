8eacdb39e70396f14e2a4fd06efdf7743ec7f4e1
Ander0399 <you@example.com>
2023-11-09 03:12:19 -0500

    vue ejemplo

---
+++ src/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="Home">
+   <head>
+    <title>ConCars Luxury</title>
+    <link rel="stylesheet" type="text/css" href="../assets/style.css">
+    <link rel="icon" type="image/png" href="../assets/logoAmp.png">
+</head>
+
+<body class="body">
+    <!-- <ul class="navbar">
+        <li class="nav-item"><a href="index.html">Inicio</a></li>
+        <li class="nav-item"><a href="html/servicios.html">Servicios</a></li>
+        <li class="nav-item"><a href="html/acerca.html">Acerca de</a></li>
+        <li class="nav-item"><a href="html/contactos.html">Contacto</a></li>
+    </ul> -->
+    <div class="center-image">
+        <img src="../assets/logo.png" alt="Logo Concar">
+    </div>
+
+    <h1>- Bienvenido a ConCars -</h1>
+
+    <div class="card-container">
+        <div class="card">
+            <img src="../assets/nissanskyline.jpg" alt="Nissan">
+            <h2>SKYLINE GT-R R34</h2>
+            <p>El Nissan Skyline GT-R R34 es un legendario automóvil deportivo japonés conocido por su diseño agresivo y
+                potencia, especialmente apreciado por los amantes de los deportivos.</p>
+        </div>
+        <div class="card">
+            <img src="../assets/supra.jpg" alt="Supra">
+            <h2>TOYOTA SUPRA</h2>
+            <p>El Toyota Supra es un icónico automóvil deportivo japonés conocido por su estilo deportivo y su potente
+                motor, apreciado por entusiastas de los deportivos.</p>
+        </div>
+        <div class="card">
+            <img src="../assets/dodge.jpg" alt="Dodge Challenger">
+            <h2>DODGE CHALLENGER SRT DEMON</h2>
+            <p>El Dodge Challenger SRT Demon es un poderoso muscle car estadounidense, famoso por su desempeño extremo y
+                su motor V8 supercargado de alto rendimiento. Es reconocido por su capacidad para ofrecer una
+                experiencia de conducción emocionante y una impresionante potencia en línea recta.</p>
+        </div>
+        <div class="card">
+            <img src="../assets/bmw-m2.jpg" alt="Bmw">
+            <h2>BMW M2</h2>
+            <p>El Bmw M2 es la nueva entrega del deportivo más compacto de la familia M con su fórmula de seis cilindros
+                y tracción trasera ya está aquí con 460 CV y un diseño con identidad propia.</p>
+        </div>
+        <div class="card">
+            <img src="../assets/ferrarikc23.jpg" alt="Ferrari">
+            <h2>FERRARI KC23</h2>
+            <p>El KC23 es un Proyecto Especial de Ferrari creado a partir del Ferrari 488 GT3 Evo del año 2020, tiene un
+                motor V8 con una potencia que no ha trascendido.</p>
+        </div>
+        <div class="card">
+            <img src="../assets/toyota-gr86.jpg" alt="Toyota">
+            <h2>TOYOTA GR86</h2>
+            <p>El GR86 es un deportivo de tamaño compacto, fabricado por Toyota desde 2012. Actualmente se comercializa
+                la segunda generación, que tiene como principal novedad el cambio de denominación sufrido</p>
+        </div>
+    </div>
+    <div>
+        <footer>
+            <p>&copy; 2023 Concesionario de Autos ConCars</p>
+        </footer>
+    </div>
+
+</body>
+
+  </div>
+</template>
+
